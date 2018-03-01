5d1bdd5970ac957da9aaf0aed59bd8d1e0e734a4
Nayssi <yassin.hamaoui@polytechnique.edu>
2018-03-01 22:00:15 +0100

    blabla

--- Voiture.java
+++ Voiture.java
@@ -30,10 +30,30 @@ public class Voiture implements Comparable{
 	public void newCourse(Course course) {
 		int start=this.dispo+Math.abs(course.debut.x-this.courseCourante.fin.x)+Math.abs(course.debut.y-this.courseCourante.fin.y);
 		this.courseCourante=course;
-		this.dispo=start+Math.abs(course.fin.x-course.debut.x)+Math.abs(course.fin.y-course.debut.y);	
+		this.dispo=Math.max(start, course.debut.t)+Math.abs(course.fin.x-course.debut.x)+Math.abs(course.fin.y-course.debut.y);	
 		nbreCourses=nbreCourses+1;
 		coursesEffectuees=coursesEffectuees+course.id+" ";
 
+	}
+	
+	public int isCourse(Course course) {
+		int start=this.dispo+Math.abs(course.debut.x-this.courseCourante.fin.x)+Math.abs(course.debut.y-this.courseCourante.fin.y);
+		int finPrevu=Math.max(start, course.debut.t)+Math.abs(course.fin.x-course.debut.x)+Math.abs(course.fin.y-course.debut.y);
+		if (start<=course.debut.t) {
+			return 2;
+		}
+		else {
+			if (finPrevu<course.fin.t) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}	
+	}
+	
+	public int getDist(Course course) {
+		return Math.abs(course.debut.x-this.courseCourante.fin.x)+Math.abs(course.debut.y-this.courseCourante.fin.y);
 	}
 	
 

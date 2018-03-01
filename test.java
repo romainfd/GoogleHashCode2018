4f5db6cf9751942bf5bcc58c1a89d0c7931f1195
Romain Fouilland <romain.fouilland@polytechnique.edu>
2018-03-01 20:56:14 +0100

    test sur tout

--- test.java
+++ test.java
@@ -5,31 +5,35 @@ import java.util.PriorityQueue;
 public class test {
 
 	public static void main(String[] args) {
+		String[] inputs = new String[] {"a_example.in", "b_should_be_easy.in", "c_no_hurry.in", "d_metropolis.in", "e_high_bonus.in"};
 		Course c = new Course(0, "0 0 1 3 2 9");
 		System.out.println(c);
 		System.out.println(c.debut);
 		System.out.println(c.debut.x);
 		
-		int currentTime = 0;
-		Instance ins = new Instance("a_example.in");
-		PriorityQueue<Course> courses=ins.courses;
-		PriorityQueue<Voiture> voitures= new PriorityQueue<Voiture>();
-		
-		for(int i=0;i<ins.f;i++){
-			voitures.add(new Voiture());
+		for (int j = 0; j < 5; j++) {
+			
+			int currentTime = 0;
+			Instance ins = new Instance(inputs[j]);
+			PriorityQueue<Course> courses=ins.courses;
+			PriorityQueue<Voiture> voitures= new PriorityQueue<Voiture>();
+			
+			for(int i=0;i<ins.f;i++){
+				voitures.add(new Voiture());
+			}
+			
+			Course c1;
+			Voiture v1;
+			while (!courses.isEmpty()){
+				v1=voitures.poll();
+				currentTime=v1.dispo;
+				c1=courses.poll();
+				v1.newCourse(c1);
+				voitures.add(v1);
+			}
+			
+			ins.output(voitures);
 		}
-		
-		Course c1;
-		Voiture v1;
-		while (!courses.isEmpty()){
-			v1=voitures.poll();
-			currentTime=v1.dispo;
-			c1=courses.poll();
-			v1.newCourse(c1);
-			voitures.add(v1);
-		}
-		
-		ins.output(voitures);
 	}
 
 }
